83c635ba3f5a444ee9bfa7ce67bd349f49acd024
ElliotBachman <elliotbachmanx@gmail.com>
2024-08-27 14:05:22 -0500

    Small JS updates

--- scripts/index.js
+++ scripts/index.js
@@ -37,6 +37,23 @@ const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
 );
 
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  console.log(data);
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  //select the image element
+
+  cardNameEl.textContent = data.name;
+  //assgn values to the image src and alt
+  return cardElement;
+}
+
 function openModal() {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
@@ -57,3 +74,8 @@ function handleEditFormSubmit(event) {
 profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
